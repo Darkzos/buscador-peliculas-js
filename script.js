71be6aefa752a5f83773c2029671faa2c38c1441
Hebert Aranda Gutierrez <112402820+Darkzos@users.noreply.github.com>
2023-12-05 16:36:27 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+
+document.getElementById("searchButton").addEventListener('click', searchMovies)
+
+
+let api_key ="cd50afb7aa8cfcbe9ea6c638eabc0268"
+let urlBase = 'https://api.themoviedb.org/3/search/movie'
+let urlImg = 'https://image.tmdb.org/t/p/w500'
+
+let resultContainer = document.getElementById("results")
+
+function searchMovies() {
+    resultContainer.innerHTML = 'Cargando....'
+    let searchInput = document.getElementById("searchInput").value
+    fetch(`${urlBase}?api_key=${api_key}&query=${searchInput}`)
+    .then(response => response.json())
+    .then(response => displayMovies(response.results))
+
+};
+
+function displayMovies(movies){
+    resultContainer.innerHTML = ''
+
+    if(movies.length === 0) {
+        resultContainer.innerHTML = "<p> No se encontraron resultados para tu búsqueda </p>"
+        return
+    }
+
+    movies.forEach(movie => {
+        let movieDiv = document.createElement('div')
+        movieDiv.classList.add('movie')
+
+        let title = document.createElement('h2')
+        title.textContent = movie.title
+
+        let releaseDate = document.createElement('p')
+        releaseDate.textContent = "La fecha de lanzamiento fue: " + movie.release_date
+    
+        let overview = document.createElement('p')
+        overview.textContent = movie.overview
+
+        let posterPath = urlImg + movie.poster_path
+        let poster = document.createElement('img')
+        poster.src= posterPath
+
+        let voteAverage = document.createElement('p')
+        voteAverage.textContent = "La calificación promedio para esta pelicula es de: " + movie.vote_average.toFixed(1)
+        
+        let score = movie.vote_average.toFixed(1)
+        let stars = ''
+
+        switch (true) {
+            case score > 0 && score <= 2:
+                stars = "*" 
+                break;
+            case score > 2 && score <= 4:
+                stars = '**'
+                break;
+            case score > 4 && score <= 6:
+                stars = '***'
+                break;
+            case score > 6 && score <= 8:
+                stars = '****'
+                break;
+            case score > 8 && score <= 10:
+                stars = '*****'
+                break;
+            default:
+                break;
+        };
+
+        let scoreStars = document.createElement('p')
+        scoreStars.textContent = "El rating promedio en estrellas es: " + stars
+
+        movieDiv.appendChild(poster)
+        movieDiv.appendChild(title)
+        movieDiv.appendChild(releaseDate)
+        movieDiv.appendChild(overview)
+        movieDiv.appendChild(voteAverage)
+        movieDiv.appendChild(scoreStars)
+
+        resultContainer.appendChild(movieDiv)
+    });
+};
